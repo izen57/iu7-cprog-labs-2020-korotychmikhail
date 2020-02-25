a3c7c046166a60bdac9175f93aa754b9b1dea429
kmd19u787 <861-kmd-19u787@users.noreply.git.iu7bmstu.ru>
2020-02-25 15:50:00 +0300

    lab and test were added.

---
+++ lab_01_1_7/lab_01_1_7.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+int fact(int n)
+{
+	int res = 1;
+	for (int i = 1; i <= n; i++)
+		res *= i;
+	return res;
+}
+int main()
+{
+	float x, eps;
+	if (scanf("%f %e", &x, &eps) == 2)
+	{
+		float f, s = 0;
+		int i = 1;
+		float curr = pow(-1, i) * pow(x, 2 * i + 1) / fact(2 * i + 1);
+		float next = pow(-1, 2 * i + 1) * pow(x, 2 * i + 2) / fact(2 * i + 2);
+		do
+		{
+			s += curr;
+			curr = next;
+			next = pow(-1, 2 * i + 1) * pow(x, 2 * i + 2) / fact(2 * i + 2);
+			i++;
+		} while (fabs(next - curr) > eps);
+		f = sin(x);
+		printf("%.6f %.6f %.6f %.6f", s, f, fabs(f - s), fabs((f - s) / f));
+		return 0;
+	}
+	else
+	{
+		printf("Input error.");
+		return 1;
+	}
+}
