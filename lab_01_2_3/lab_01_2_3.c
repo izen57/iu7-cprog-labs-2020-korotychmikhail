0791487fc7de22503141c5fcc0ce34c72974cebc
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-02-25 16:24:33 +0300

    Delete lab_01_2_3.c

--- lab_01_2_3/lab_01_2_3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	float r1, r2, r3;
-	scanf("%f %f %f", &r1, &r2, &r3);
-	float r = r1 * r2 *r3 / (r2 * r3 + r1 * r3 + r1 * r2);
-	printf("%.5f", r);
-	return 0;
-}
