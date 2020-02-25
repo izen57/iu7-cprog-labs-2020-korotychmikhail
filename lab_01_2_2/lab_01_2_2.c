936ccfc6c63d85726d016bbdbea1e5cf3a23461e
kmd19u787 <861-kmd-19u787@users.noreply.git.iu7bmstu.ru>
2020-02-25 16:01:00 +0300

    math.h was added

--- lab_01_2_2/lab_01_2_2.c
+++ lab_01_2_2/lab_01_2_2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 const float pi = 3.14159;
 int main()
@@ -6,7 +7,7 @@ int main()
 	float x1, y1, x2, y2, x3, y3;
 	scanf("%f %f %f %f %f %f", &x1, &y2, &x2, &y2, &x3, &y3);
 	float ab = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-	float bc  = sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+	float bc = sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
 	float ac = sqrt((x3 - x1) * (x3 - x1) + (y3 - y1) * (y3 - y1));
 	float p = ab + bc + ac;
 	printf("%.5f", p);
