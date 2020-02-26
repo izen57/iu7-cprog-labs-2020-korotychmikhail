978cb14e8b3b0e229230aabd4f79c69b3c703d28
kmd19u787 <861-kmd-19u787@users.noreply.git.iu7bmstu.ru>
2020-02-26 22:13:40 +0300

    bugs were fixed

--- lab_01_1_7/lab_01_1_7.c
+++ lab_01_1_7/lab_01_1_7.c
@@ -21,13 +21,13 @@ int main()
 		int i = 1;
 		float curr = x;
 		float next = pow(-1, i + 1) * pow(x, 2 * i + 2) / fact(2 * i + 2);
-		do
+		while (fabs(curr) > eps)
 		{
 			s += curr;
 			curr = next;
 			next = pow(-1, i + 2) * pow(x, 2 * i + 3) / fact(2 * i + 3);
 			i++;
-		} while (fabs(curr) > eps);
+		} 
 		f = sin(x);
 		printf("%.6f %.6f %.6f %.6f", s, f, fabs(f - s), fabs((f - s) / f));
 		return 0;
