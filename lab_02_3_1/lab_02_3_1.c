545bae4c9c24c56c1773272eb7db0582f6119eba
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-03-14 22:37:54 +0300

    fixes

--- lab_02_3_1/lab_02_3_1.c
+++ lab_02_3_1/lab_02_3_1.c
@@ -18,7 +18,7 @@ status_code read_array(int (*a)[ARRAY_LENGHT], int *n)
 		count += scanf("%d", &(*a)[i]);
 	return success;
 }
-status_code geometry_mean(int a[ARRAY_LENGHT], int n, int *geomean)
+status_code geometry_mean(int a[ARRAY_LENGHT], int n, float *geomean)
 {
 	*geomean = 1;
 	int count = 0;
@@ -45,11 +45,11 @@ int main()
 		printf("Input error.");
 		return error;
 	}
-	int geomean;
+	float geomean;
 	error = geometry_mean(a, n, &geomean);
 	if (error)
 		printf("Input error.");
 	else
-		printf("%d", geomean);
+		printf("%.5f", geomean);
 	return error;
 }
