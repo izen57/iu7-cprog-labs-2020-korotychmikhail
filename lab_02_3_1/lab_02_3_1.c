649d10e6ef93d95ab344cc827efc71462b886aa4
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-03-14 18:57:19 +0300

    lab's file and out_1.txt were added.

---
+++ lab_02_3_1/lab_02_3_1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define N 10
+typedef enum = {SUCCESS = 0, INCORRECT_INPUT} status_code;
+status_code read_array(int a[N], int *n)
+{
+	int count = 0;
+	if (scanf("%d", n) != 1)
+		return INCORRECT_INPUT;
+	if (*n <= 0 || *n > N)
+		return INCORRECT_INPUT;
+	for (int i = 0; i < *n; i++)
+		count += scanf("%d", a[i]);
+	return SUCCESS;
+}
+status_code geometry_mean(int a[N], int n, int *geomean)
+{
+	*geomean = 1;
+	for (int i = 0, int count = 0; i < n; i++, count++)
+		if (a[i] > 0)
+			*geomean *= a[i];
+	if (count == 0)
+		return INCORRECT_INPUT;
+	*geomean = sqrt(*geomean);
+	return SUCCESS;
+}
+int main()
+{
+	int a[N], n;
+	status_code error = read_array(a, &n);
+	if (error)
+	{
+		printf("Input error.");
+		return error;
+	}
+	float geomean;
+	error = geometry_mean(a, n, &geomean);
+	if (error)
+	{
+		pritnf("Input error.");
+		return INCORRECT_INPUT;
+	}
+	return SUCCESS;
+}
