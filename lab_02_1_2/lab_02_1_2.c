3c365d4a5286c987937b88071850f6f9dba9df31
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-03-16 12:50:12 +0300

    fix

--- lab_02_1_2/lab_02_1_2.c
+++ lab_02_1_2/lab_02_1_2.c
@@ -7,17 +7,14 @@ typedef enum
 	incorrect_input,
 	unsiutable_condition
 } status_code;
-int if_start_is_end(int n)
+int dig_is_simple(int n)
 {
-	n = fabs(n);
-	int first, ost, last = n % 10;
-	while (n > 0)
-	{
-		ost = n % 10;
-		n /= 10;
-	}
-	first = ost;
-	return first == last;
+	if (n < 2)
+		return 0;
+	for(int i = 2; i < n; i++)
+		if (n % i == 0)
+			return 0;
+	return 1;
 }
 status_code read_array(int (*a)[ARRAY_LENGHT], int *n)
 {
@@ -34,12 +31,12 @@ status_code read_array(int (*a)[ARRAY_LENGHT], int *n)
 		return incorrect_input;
 	return success;
 }
-status_code samedig(int a[ARRAY_LENGHT], int (*b)[ARRAY_LENGHT], int n, int *count)
+status_code calc(int a[ARRAY_LENGHT], int (*b)[ARRAY_LENGHT], int n, int *count)
 {
 	*count = 0;
 	int j = 0;
 	for (int i = 0; i < n; i++)
-		if (if_start_is_end(a[i]))
+		if (dig_is_simple(a[i]))
 		{
 			(*count)++;
 			(*b)[j] = a[i];
@@ -60,7 +57,7 @@ int main()
 		return error;
 	}
 	int b[ARRAY_LENGHT], count;
-	error = samedig(a, &b, n, &count);
+	error = calc(a, &b, n, &count);
 	if (error)
 		printf("There are no such digits.");
 	else
