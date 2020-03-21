9db5aca58726bde03e2ff6af5f02999b649ecd17
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-03-21 21:33:06 +0300

    lab's file was added.

--- lab_02_4_5/lab_02_4_5.c
+++ lab_02_4_5/lab_02_4_5.c
@@ -20,14 +20,27 @@ status_code read_array(int **begin, int **end)
 	int rc = scanf("%c", &tmp);
 	if (count != n || rc != EOF || *begin == *end)
 		return incorrect_input;
-	printf("%d", n);
 	return success;
+}
+void check_array(int **begin, int **end)
+{
+	for (int *i = *begin; i < *end; i++)
+		for (int *j = i + 1; j < *end; j++)
+		if (*i == *j)
+			*i = -1;
 }
 int main()
 {
 	int a[ARRAY_LENGHT], *begin = a, *end = a;
 	status_code error = read_array(&begin, &end);
 	if (error)
+	{
 		printf("Input error.");
-	return error;
+		return error;
+	}
+	int *pa = begin;
+	check_array(&begin, &end);
+	for (int *i = pa; i < end; i++)
+		if (*i != -1)
+			printf("%d ", *i);
 }
