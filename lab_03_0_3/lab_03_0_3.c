f496e7929d342706add768a868d6043eda1ea8b6
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-04-06 15:47:08 +0300

    fix

--- lab_03_0_3/lab_03_0_3.c
+++ lab_03_0_3/lab_03_0_3.c
@@ -7,11 +7,8 @@ typedef enum
 } status_code;
 status_code read_array(int a[ARRAY_LENGHT][ARRAY_LENGHT], int *n, int *m)
 {
-	if (scanf("%d %d", n, m) != 2 || *n < 1 && *m < 1 || *n > ARRAY_LENGHT || *m > ARRAY_LENGHT)
+	if (scanf("%d %d", n, m) != 2 || *n < 1 || *m < 1 || *n > ARRAY_LENGHT || *m > ARRAY_LENGHT)
 		return incorrect_input;
-	if ((*n < 1 && *m < 1) || *n > ARRAY_LENGHT || *m > ARRAY_LENGHT)
-		return incorrect_input;
-	int count = 0;
 	for (int i = 0; i < *n; i++)
 		for (int j = 0; j < *m; j++)
 			if (scanf("%d", &a[i][j]) == 1)
