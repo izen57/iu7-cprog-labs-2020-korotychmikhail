6a349c6d2ce680b26994646adf459aabb7c50c71
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-04-21 11:55:13 +0300

    lab_04

---
+++ lab_04_1/my_strcspn.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "my_strcspn.h"
+void my_strcspn(char a[LINE_LENGHT + 1], char b[LINE_LENGHT + 1], int n, int m)
+{
+	int flag = 0;
+	for (int i = 0; a[i] != '\0'; i++)
+		for (int j = 0; b[j] != '\0'; j++)
+			if (a[i] == b[j])
+			{
+				flag = 1;
+				printf("%d", i);
+				break;
+			}
+	if (!flag)
+		printf("%d", LINE_LENGHT);
+}
