6ea69b94c127dd5d0abf71557be803f9c5d85a56
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-04-21 20:04:28 +0300

    fix

--- lab_04_1/my_strcspn.c
+++ lab_04_1/my_strcspn.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "my_strcspn.h"
 void my_strcspn(char a[LINE_LENGHT + 1], char b[LINE_LENGHT + 1])
@@ -6,19 +7,14 @@ void my_strcspn(char a[LINE_LENGHT + 1], char b[LINE_LENGHT + 1])
 	if (a[0] == '\0' || b[0] == '\0' || a[0] == '\n' || b[0] == '\n')
 		EXIT_FAILURE;
 	int flag = 0;
-	for (int i = 0; a[i] != '\0'; i++)
-		for (int j = 0; b[j] != '\0'; j++)
-			if (a[j] == b[i])
+	for (int i = 0; b[i] != '\0'; i++)
+		for (int j = 0; a[j] != '\0'; j++)
+			if (b[i] == a[j])
 			{
 				flag = 1;
-				printf("%d", j);
+				printf("%d", i);
 				EXIT_SUCCESS;
 			}
 	if (!flag)
-	{
-		int count = 0;
-		for (int i = 0; b[i] != '\0'; i++)
-			count++;
-		printf("%d", count);
-	}
+		printf("%d", strlen(a));
 }
