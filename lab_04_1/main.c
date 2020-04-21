1c916883b6a65e69137d871bf1ead0bf90648225
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-04-21 16:42:18 +0300

    fix

--- lab_04_1/main.c
+++ lab_04_1/main.c
@@ -1,12 +1,11 @@
-#define __USE_MINGW_ANSI_STDIO 1 
+#define __USE_MINGW_ANSI_STDIO 1
 #include <stdio.h>
 #include <string.h>
 #include "my_strcspn.h"
 int main(void)
 {
-	char a[LINE_LENGHT + 1];
-	char b[LINE_LENGHT + 1];
+	char a[LINE_LENGHT + 1], b[LINE_LENGHT + 1];
 	printf("%ld\n", strcspn(a, b));
-	status_code error = my_strcspn(a, b);
-	return error;
+	my_strcspn(a, b);
+	return 0;
 }
--- lab_04_1/my_strcspn.h
+++ lab_04_1/my_strcspn.h
@@ -1,10 +1,5 @@
 #ifndef __MY__STRCSPN__H__
 #define __MY__STRCSPN__H__
 #define LINE_LENGHT 256
-typedef enum
-{
-	success,
-	incorrect_input
-} status_code;
-status_code my_strcspn(char a[LINE_LENGHT + 1], char b[LINE_LENGHT + 1]);
+void my_strcspn(char a[LINE_LENGHT + 1], char b[LINE_LENGHT + 1]);
 #endif
