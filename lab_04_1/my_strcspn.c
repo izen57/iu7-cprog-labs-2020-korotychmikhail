c856c4ec17ca19997144bf336e9ecc7148c46a28
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-04-21 12:18:45 +0300

    fix

--- lab_04_1/my_strcspn.c
+++ lab_04_1/my_strcspn.c
@@ -3,8 +3,8 @@
 void my_strcspn(char a[LINE_LENGHT + 1], char b[LINE_LENGHT + 1], int n, int m)
 {
 	int flag = 0;
-	for (int i = 0; a[i] != '\0'; i++)
-		for (int j = 0; b[j] != '\0'; j++)
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++)
 			if (a[i] == b[j])
 			{
 				flag = 1;
