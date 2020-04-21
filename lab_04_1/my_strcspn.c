335ae982df39e115e301da137006cd626c093d22
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-04-21 19:49:25 +0300

    fix

--- lab_04_1/my_strcspn.c
+++ lab_04_1/my_strcspn.c
@@ -8,10 +8,10 @@ void my_strcspn(char a[LINE_LENGHT + 1], char b[LINE_LENGHT + 1])
 	int flag = 0;
 	for (int i = 0; a[i] != '\0'; i++)
 		for (int j = 0; b[j] != '\0'; j++)
-			if (a[i] == b[j])
+			if (a[j] == b[i])
 			{
 				flag = 1;
-				printf("%d", i);
+				printf("%d", j);
 				EXIT_SUCCESS;
 			}
 	if (!flag)
