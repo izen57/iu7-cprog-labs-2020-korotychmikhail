9a8cf06a1e8f774f68715c0c5c415fde113e0f51
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-04-21 17:50:39 +0300

    fix

--- lab_04_1/my_strcspn.c
+++ lab_04_1/my_strcspn.c
@@ -3,6 +3,9 @@
 #include "my_strcspn.h"
 void my_strcspn(char a[LINE_LENGHT + 1], char b[LINE_LENGHT + 1])
 {
+	int count = 0;
+	for (int i = 0; a[i] != 0; i++)
+		count++;
 	int flag = 0;
 	for (int i = 0; a[i] != '\0'; i++)
 		for (int j = 0; b[j] != '\0'; j++)
@@ -10,7 +13,8 @@ void my_strcspn(char a[LINE_LENGHT + 1], char b[LINE_LENGHT + 1])
 			{
 				flag = 1;
 				printf("%d", i);
+				break;
 			}
 	if (!flag)
-		printf("%ld", strlen(*a));
+		printf("%d", count);
 }
