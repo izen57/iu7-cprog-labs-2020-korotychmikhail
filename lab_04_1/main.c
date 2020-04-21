8c20547d527635a728a9df60c0880d9210728e07
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-04-21 18:25:45 +0300

    fix

--- lab_04_1/main.c
+++ lab_04_1/main.c
@@ -1,11 +1,12 @@
 #define __USE_MINGW_ANSI_STDIO 1
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "my_strcspn.h"
 int main(void)
 {
 	char a[LINE_LENGHT + 1], b[LINE_LENGHT + 1];
 	printf("%ld\n", strcspn(a, b));
 	my_strcspn(a, b);
-	return 0;
+	EXIT_SUCCESS;
 }
--- lab_04_1/my_strcspn.c
+++ lab_04_1/my_strcspn.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include "my_strcspn.h"
 void my_strcspn(char a[LINE_LENGHT + 1], char b[LINE_LENGHT + 1])
 {
+	if (a[0] == '\0' || b[0] == '\0')
+		EXIT_FAILURE;
 	int flag = 0;
 	for (int i = 0; a[i] != '\0'; i++)
 		for (int j = 0; b[j] != '\0'; j++)
