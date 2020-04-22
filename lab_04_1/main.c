ddc82d1a5dd9d12ce46ec4880431ee843fce9083
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-04-22 18:07:38 +0300

    fix

--- lab_04_1/main.c
+++ lab_04_1/main.c
@@ -4,19 +4,5 @@ int main(void)
 {
 	if (my_strcspn("", "") != 0)
 		return EXIT_FAILURE;
-	/*if (my_strcspn("\n", "\n") != 0)
-		return EXIT_FAILURE;
-	if (my_strcspn("dep", "") != 3)
-		return EXIT_FAILURE;
-	if (my_strcspn("aaa\nffw", "frvr") != 4)
-		EXIT_FAILURE;
-	if (my_strcspn("aaa\0bbb", "") != 3)
-		return EXIT_FAILURE;
-	if (my_strcspn("sss\0frr", "rff") != 3)
-		return EXIT_FAILURE;
-	if (my_strcspn("abcde f", " gfr") != 5)
-		return EXIT_FAILURE;
-	if (my_strcspn("1736", "0000") != 4)
-		return EXIT_FAILURE;*/
 	return EXIT_SUCCESS;
 }
