4fab0d3d1875bb8f58d44377819f3223c71f5b26
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-04-22 13:39:54 +0300

    fix

--- lab_04_1/my_strcspn.c
+++ lab_04_1/my_strcspn.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 #include "my_strcspn.h"
 int my_strcspn(char *a, char *b)
 {
+	int res;
 	if (a[0] == '\0' || b[0] == '\0' || a[0] == '\n' || b[0] == '\n')
-		return 0;
+		res = 0;
 	int flag = 0;
 	for (int i = 0; a[i] != '\0'; i++)
 		for (int j = 0; b[j] != '\0'; j++)
 			if (a[i] == b[j])
 			{
 				flag = 1;
-				return i;
+				res = i;
+				break;
 			}
 	if (!flag)
-	{
-		int count = 0;
-		for (int i = 0; a[i] != '\0'; i++)
-			count++;
-		return count;
-	}
+		res = strlen(a);
+	return res;
 }
