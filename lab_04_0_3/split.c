50fef73841415d2a15b65bb67dd1ca18c1a93300
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-04-27 15:07:28 +0300

    fix

---
+++ lab_04_0_3/split.c
@@ -0,0 +1,43 @@
+#include "split.h"
+int split_line(char *line, char splited_line[LINE_LENGHT / 2 + 1][WORD_LENGHT])
+{
+	int count_of_letters = 0, count_of_splits = 1, result = 0;
+	char word[WORD_LENGHT];
+	for (int i = 0; line[i] != '\0'; i++)
+	{
+		if (line[i] != '\n' && line[i] != '\0' && line[i] != '\t' && line[i] != ',' && line[i] != ';' && line[i] != ':' && line[i] != '-' && line[i] != '.' && line[i] != '!' && line[i] != '?' && line[i] != ' ')
+		{
+			word[count_of_letters] = line[i];
+			count_of_splits = 0;
+			count_of_letters++;
+		}
+		else
+		{
+			if (count_of_splits == 0)
+			{
+				count_of_splits++;
+				for (int j = 0; j < count_of_letters; j++)
+				{
+					splited_line[result][j] = word[j];
+					word[j] = '\0';
+				}
+				for (int j = count_of_letters; j < WORD_LENGHT; j++)
+					splited_line[result][j] = '\0';
+				result++;
+				count_of_letters = 0;
+			}
+		}
+	}
+	if (count_of_letters > 0)
+	{
+		for (int j = 0; j < count_of_letters; j++)
+		{
+			splited_line[result][j] = word[j];
+			word[j] = '\0';
+		}
+		for (int j = count_of_letters; j < WORD_LENGHT; j++)
+			splited_line[result][j] = '\0';
+		result++;
+	}
+	return result;
+}
