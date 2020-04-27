27757dad982f781ba8adc4a03bc7e262df3813f7
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-04-27 19:03:52 +0300

    fix

--- lab_04_0_3/main.c
+++ lab_04_0_3/main.c
@@ -53,7 +53,6 @@ int main(void)
 		return check_line(line);
 	char splited_line[LINE_LENGHT / 2 + 1][WORD_LENGHT];
 	int count_of_words = split_line(line, splited_line);
-	line_processing(splited_line, count_of_words);
 	char new_str[LINE_LENGHT / 2 + 1][WORD_LENGHT];
 	for (int i = count_of_words - 2; i >= 0; i--)
 		if (!strcmp(splited_line[i], splited_line[count_of_words - 1]))
