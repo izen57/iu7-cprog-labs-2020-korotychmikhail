d787c3e1bc3e977f9879d7cd6eff27b0f776821b
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-04-27 20:11:22 +0300

    fix

--- lab_04_0_3/main.c
+++ lab_04_0_3/main.c
@@ -56,7 +56,7 @@ status_code check_line(char *line)
 			else
 				count_of_letters = 0;
 		}
-		if (count_of_symbols == 0 || flag || count_of_symbols == 16)
+		if (count_of_symbols == 0 || flag ||)
 			return incorrect_input;
 		else
 			return success;
@@ -72,8 +72,8 @@ int main(void)
 		return check_line(line);
 	char splited_line[LINE_LENGHT / 2 + 1][WORD_LENGHT];
 	int count_of_words = split_line(line, splited_line);
-	/*if (count_of_words < 2)
-		return incorrect_input;*/
+	if (count_of_words < 1)
+		return incorrect_input;
 	line_processing(splited_line, count_of_words);
 	return success;
 }
