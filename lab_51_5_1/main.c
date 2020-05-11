110741ce52dfd613b2122b44193b2453498a3164
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-05-11 15:20:50 +0300

    fix

--- lab_51_5_1/main.c
+++ lab_51_5_1/main.c
@@ -7,10 +7,10 @@ int process(FILE *file, int *max)
 	int count = 1, prev, curr;
 	if (fscanf(file, "%d", &prev) == 1)
 	{
-		int flag = 0;
+		//int flag = 0;
 		while (fscanf(file, "%d", &curr) == 1)
 		{
-			flag = 1;
+			//flag = 1;
 			if (curr == prev)
 				count++;
 			else
