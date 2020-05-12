8d109886d048c533c36e7bae8f73b991a81c95d9
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-05-12 15:57:55 +0300

    2nd task

---
+++ lab_51_2_2/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <errno.h>
+#define SUCCESS 0
+#define INCORRECT_INPUT -1
+int avg_function(FILE *file, float *avg, int *count)
+{
+	float num, sum = 0;
+	while (fscanf(file, "%f", &num) == 1)
+	{
+		sum += num;
+		(*count)++;
+	}
+	if (*count <= 1)
+		return INCORRECT_INPUT;
+	else
+	{
+		*avg = sum / *count;
+		return SUCCESS;
+	}
+}
+void disp_function(FILE *file, float *disp, float avg, int count)
+{
+	float num, sum = 0;
+	for (int i = 0; i < count; i++)
+	{
+		fscanf(file, "%f", &num);
+		sum += (num - avg) * (num - avg);
+	}
+	*disp = sum / count;
+}
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "Not enough options: %s", strerror(errno));
+		return INCORRECT_INPUT;
+	}
+	FILE *file;
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Unable to open file: %s", strerror(errno));
+		return INCORRECT_INPUT;
+	}
+	float avg;
+	int count = 0, error = avg_function(argv[1], &avg, &count);
+	if (error)
+	{
+		fprintf(stderr, "Incorrect input: %s", strerror(errno));
+		return error;
+	}
+	float disp;
+	disp_function(argv[1], &disp, avg, count);
+	fprintf(stdout, "%f", disp);
+	return SUCCESS;
+}
