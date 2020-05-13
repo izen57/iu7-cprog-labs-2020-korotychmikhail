701529809fb12dd2c002c98b390c5ae7dcd942a1
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-05-13 16:47:41 +0300

    fix

--- lab_51_2_2/main.c
+++ lab_51_2_2/main.c
@@ -31,11 +31,6 @@ void disp_function(FILE *file, float *disp, float avg, int count)
 }
 int main(int argc, char **argv)
 {
-	/*if (argc != 2)
-	{
-		fprintf(stderr, "Not enough options: %s", strerror(errno));
-		return INCORRECT_INPUT;
-	}*/
 	FILE *file;
 	file = fopen(argv[1], "r");
 	if (!file)
