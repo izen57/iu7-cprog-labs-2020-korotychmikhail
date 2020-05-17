5d69765e5a7416b52721d1eb2d9d813e3d3a1209
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-05-17 16:58:03 +0300

    fix

--- lab_51_3/main.c
+++ lab_51_3/main.c
@@ -2,15 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #define SUCCESS 0
-#define INCORRECT_INPUT -1
+#define INCORRECT_INPUT 1
 int creat_numbers(char *path, int number)
 {
 	if (number < 1)
 		return INCORRECT_INPUT;
 	FILE *file;
 	file = fopen(path, "wb");
-	/*if (!file)
-		return INCORRECT_INPUT;*/
 	for (int i = 0; i < number; i++)
 	{
 		int val = rand() % (number * 2);
