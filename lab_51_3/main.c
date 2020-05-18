25716ffe391320f8ad1a4564c9cdc09303a34232
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-05-18 21:01:58 +0300

    fix

--- lab_51_3/main.c
+++ lab_51_3/main.c
@@ -26,18 +26,18 @@ int file_size(FILE *file, size_t *size)
 	if (ftell(file) < 0)
 		return INCORRECT_INPUT;
 	*size = ftell(file);
-	fseek(file, 0L, SEEK_SET);
-	return SUCCESS;
+	return fseek(file, 0L, SEEK_SET);
 }
-int print_numbers(char *path, size_t *size)
+int print_numbers(char *path)
 {
 	FILE *file;
 	file = fopen(path, "rb");
 	if (!file)
 		return INCORRECT_INPUT;
 	int val;
-	if (!file_size(file, size))
-		for (int i = 0; i < *size / sizeof(int); i++)
+	size_t size;
+	if (!file_size(file, &size))
+		for (int i = 0; i < size / sizeof(int); i++)
 		{
 			if (fread(&val, sizeof(int), 1, file) == 1)
 				printf("%d ", val);
@@ -60,19 +60,20 @@ void put_number_by_pos(FILE *file, int *num, int pos)
 	fseek(file, pos, SEEK_SET);
 	fwrite(num, sizeof(int), 1, file);
 }
-int sort_numbers(char *path, size_t *size)
+int sort_numbers(char *path)
 {
 	FILE *file;
 	int flag, temp, num;
 	file = fopen(path, "rb+");
 	if (!file)
 		return INCORRECT_INPUT;
-	if (!file_size(file, size))
+	size_t size;
+	if (!file_size(file, &size))
 	{
 		do
 		{
 			flag = 0;
-			for (int i = 1, byte = 4; i < *size / sizeof(int); i++, byte += 4)
+			for (int i = 1, byte = 4; i < size / sizeof(int); i++, byte += 4)
 				if (get_number_by_pos(file, byte) < get_number_by_pos(file, byte - 4))
 				{
 					temp = get_number_by_pos(file, byte);
@@ -90,13 +91,12 @@ int sort_numbers(char *path, size_t *size)
 }
 int main(int argc, char **argv)
 {
-	size_t size;
-	if (!strcmp(argv[1], "c"))
+	if (!strcmp(argv[1], "c") && argc == 4)
 		return creat_numbers(argv[3], *argv[2] - '0');
-	else if (!strcmp(argv[1], "p"))
-		return print_numbers(argv[2], &size);
-	else if (!strcmp(argv[1], "s"))
-		return sort_numbers(argv[2], &size);
+	else if (!strcmp(argv[1], "p") && argc == 3)
+		return print_numbers(argv[2]);
+	else if (!strcmp(argv[1], "s") && argc == 3)
+		return sort_numbers(argv[2]);
 	else
 		return INCORRECT_INPUT;
 }
