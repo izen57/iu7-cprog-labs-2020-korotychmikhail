3abc49a38c0b25aced925344eb6c31be84f15728
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-05-28 21:17:51 +0300

    fix

--- lab_52_2/main.c
+++ lab_52_2/main.c
@@ -151,7 +151,7 @@ int add_good(char *input_output_file, information goods[LEN_STRUCT], int *n)
 	int flag = 0;
 	for (int i = 0; i < *n - 1; i++)
 	{
-		if ((new_good.cost < goods[i].cost && new_good.cost > goods[i + 1].cost)/* || (new_good.cost == goods[i].cost && new_good.amount < goods[i].amount)*/)
+		if ((new_good.cost <= goods[i].cost && new_good.cost >= goods[i + 1].cost)/* || (new_good.cost == goods[i].cost && new_good.amount < goods[i].amount)*/)
 		{
 			shift(goods, i + 1, n);
 			goods[i + 1] = new_good;
@@ -173,12 +173,12 @@ int add_good(char *input_output_file, information goods[LEN_STRUCT], int *n)
 		fclose(inout_file);
 		return SUCCESS;
 	}
-	else if ((new_good.cost > goods[0].cost)/* || (new_good.cost == goods[0].cost && new_good.amount > goods[0].amount)*/)
+	else if ((new_good.cost >= goods[0].cost)/* || (new_good.cost == goods[0].cost && new_good.amount > goods[0].amount)*/)
 	{
 		shift(goods, 0, n);
 		goods[0] = new_good;
 	}
-	else if ((new_good.cost < goods[*n - 1].cost)/* || (new_good.cost == goods[*n - 1].cost && new_good.amount < goods[*n - 1].amount)*/)
+	else if ((new_good.cost <= goods[*n - 1].cost)/* || (new_good.cost == goods[*n - 1].cost && new_good.amount < goods[*n - 1].amount)*/)
 		goods[(*n)++] = new_good;
 	/*else if (new_good.cost == goods[*n - 1].cost && new_good.amount > goods[*n - 1].amount)
 	{
