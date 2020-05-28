6a9226d801fe6d45134b71a78363bfff93c7b9ca
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-05-28 12:33:18 +0300

    fixes

--- lab_52_2/main.c
+++ lab_52_2/main.c
@@ -176,11 +176,11 @@ int main(int argc, char **argv)
 {
 	information goods[LEN_STRUCT];
 	int n = 0;
-	if (!strcmp(argv[1], "st") && argc >= 4)
+	if (!strcmp(argv[1], "st") && argc == 4)
 		return sorting_goods(argv[2], argv[3], goods, &n);
-	if (!strcmp(argv[1], "ft") && argc >= 4)
+	if (!strcmp(argv[1], "ft") && argc == 4)
 		return find_goods(argv[2], argv[3], goods, &n);
-	if (!strcmp(argv[1], "at") && argc >= 3)
+	if (!strcmp(argv[1], "at") && argc == 3)
 		return add_good(argv[2], goods, &n);
 	return UNKNOWN_PARAMETERS;
 }
