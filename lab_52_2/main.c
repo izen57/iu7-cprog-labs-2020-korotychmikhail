b2ae60f2cf189ef63701b31ebde53a07e4751d50
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-05-29 12:03:05 +0300

    fix

--- lab_52_2/main.c
+++ lab_52_2/main.c
@@ -27,12 +27,12 @@ int read_goods(FILE *file, information goods[LEN_STRUCT], int *n)
 	{
 		if (!fgets(goods[i].name, LEN_NAME + 2, file))
 			return INCORRECT_INPUT;
-		if (goods[i].name[LEN_NAME + 1] != '\0')
+		if (goods[i].name[strlen(goods[i].name) - 1] != '\n')
 			return INCORRECT_INPUT;
 		(*n)++;
 		if (!fgets(goods[i].manufacturer, LEN_MANUFACTURER + 2, file))
 			return INCORRECT_INPUT;
-		if (goods[i].manufacturer[LEN_MANUFACTURER + 1] != '\0')
+		if (goods[i].manufacturer[strlen(goods[i].manufacturer) - 1] != '\n')
 			return INCORRECT_INPUT;
 		(*n)++;
 		if (fscanf(file, "%u\n", &goods[i].cost) != 1)
@@ -44,7 +44,8 @@ int read_goods(FILE *file, information goods[LEN_STRUCT], int *n)
 	}
 	if (*n % 4)
 		return INCORRECT_INPUT;
-	*n /= 4;
+	if (*n / 4 > LEN_STRUCT)
+		return INCORRECT_INPUT;
 	rewind(file);
 	return SUCCESS;
 }
@@ -130,14 +131,14 @@ int add_good(char *input_output_file, information goods[LEN_STRUCT], int *n)
 		new_good.name[i] = '\0';
 	if (!fgets(new_good.name, LEN_NAME + 2, stdin))
 		return INCORRECT_INPUT;
-	if (new_good.name[LEN_NAME + 1] != '\0')
+	if (new_good.name[strlen(new_good.name) - 1] != '\n')
 		return INCORRECT_INPUT;
 	count++;
 	for (int i = 0; i <= LEN_MANUFACTURER + 2; i++)
 		new_good.manufacturer[i] = '\0';
 	if (!fgets(new_good.manufacturer, LEN_MANUFACTURER + 2, stdin))
 		return INCORRECT_INPUT;
-	if (new_good.manufacturer[LEN_MANUFACTURER + 1] != '\0')
+	if (new_good.manufacturer[strlen(new_good.manufacturer) - 1] != '\n')
 		return INCORRECT_INPUT;
 	count++;
 	if (fscanf(stdin, "%u\n", &new_good.cost) != 1)
