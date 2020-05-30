72ef4bb7129e95bda96b66767c9c927ea6090801
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-05-30 15:47:13 +0300

    fix

--- lab_52_2/main.c
+++ lab_52_2/main.c
@@ -18,22 +18,22 @@ int read_goods(FILE *file, information goods[LEN_STRUCT], int *n)
 {
 	for (int i = 0; i < LEN_STRUCT; i++)
 	{
-		for (int j = 0; j <= LEN_NAME + 2; j++)
+		for (int j = 0; j <= LEN_NAME + 1; j++)
 			goods[i].name[j] = '\0';
-		for (int j = 0; j <= LEN_MANUFACTURER + 2; j++)
+		for (int j = 0; j <= LEN_MANUFACTURER + 1; j++)
 			goods[i].manufacturer[j] = '\0';
 	}
 	for (int i = 0; !feof(file); i++)
 	{
-		if (!fgets(goods[i].name, LEN_NAME + 3, file))
+		if (!fgets(goods[i].name, LEN_NAME + 2, file))
 			return INCORRECT_INPUT;
-		if (goods[i].name[LEN_NAME + 2] != '\0')
-			return INCORRECT_INPUT;
+		/*if (goods[i].name[strlen(goods[i].name)] >= LEN_NAME + 1)
+			return INCORRECT_INPUT;*/
 		(*n)++;
-		if (!fgets(goods[i].manufacturer, LEN_MANUFACTURER + 3, file))
+		if (!fgets(goods[i].manufacturer, LEN_MANUFACTURER + 2, file))
 			return INCORRECT_INPUT;
-		if (goods[i].manufacturer[LEN_MANUFACTURER + 2] != '\0')
-			return INCORRECT_INPUT;
+		/*if (goods[i].manufacturer[strlen(goods[i].manufacturer)] >= LEN_MANUFACTURER + 1)
+			return INCORRECT_INPUT;*/
 		(*n)++;
 		if (fscanf(file, "%u\n", &goods[i].cost) != 1)
 			return INCORRECT_INPUT;
@@ -42,15 +42,11 @@ int read_goods(FILE *file, information goods[LEN_STRUCT], int *n)
 			return INCORRECT_INPUT;
 		(*n)++;
 	}
-	if (*n % 4)
-		return INCORRECT_INPUT;
 	*n /= 4;
-	if (*n > LEN_STRUCT)
-		return INCORRECT_INPUT;
 	rewind(file);
 	return SUCCESS;
 }
-int sorting_goods(char *input_path, char *output_path, information goods[LEN_STRUCT], int *n)
+/*int sorting_goods(char *input_path, char *output_path, information goods[LEN_STRUCT], int *n)
 {
 	FILE *input_file = fopen(input_path, "r");
 	if (!input_file)
@@ -111,7 +107,7 @@ int find_goods(char *input_path, char *substr, information goods[LEN_STRUCT], in
 	if (!count)
 		return INCORRECT_INPUT;
 	return SUCCESS;
-}
+}*/
 void shift(information goods[LEN_STRUCT], int pos, int *n)
 {
 	(*n)++;
@@ -131,25 +127,17 @@ int add_good(char *input_output_file, information goods[LEN_STRUCT], int *n)
 	int count = 0;
 	for (int i = 0; i <= LEN_NAME + 2; i++)
 		new_good.name[i] = '\0';
-	if (!fgets(new_good.name, LEN_NAME + 3, stdin))
-		return INCORRECT_INPUT;
-	if (new_good.name[LEN_NAME + 2] != '\0')
-		return INCORRECT_INPUT;
-	count++;
+	fgets(new_good.name, LEN_NAME + 2, stdin);
+	/*if (new_good.name[strlen(new_good.name)] >= LEN_NAME + 1)
+		return INCORRECT_INPUT;*/
 	for (int i = 0; i <= LEN_MANUFACTURER + 2; i++)
 		new_good.manufacturer[i] = '\0';
-	if (!fgets(new_good.manufacturer, LEN_MANUFACTURER + 3, stdin))
-		return INCORRECT_INPUT;
-	if (new_good.manufacturer[LEN_MANUFACTURER + 2] != '\0')
-		return INCORRECT_INPUT;
-	count++;
+	fgets(new_good.manufacturer, LEN_MANUFACTURER + 2, stdin);
+	/*if (new_good.manufacturer[strlen(new_good.manufacturer)] >= LEN_MANUFACTURER + 1)
+		return INCORRECT_INPUT;*/
 	if (fscanf(stdin, "%u\n", &new_good.cost) != 1)
 		return INCORRECT_INPUT;
-	count++;
 	if (fscanf(stdin, "%u", &new_good.amount) != 1)
-		return INCORRECT_INPUT;
-	count++;
-	if (count / 4 != 1)
 		return INCORRECT_INPUT;
 	int flag = 0;
 	for (int i = 0; i < *n - 1; i++)
@@ -162,9 +150,8 @@ int add_good(char *input_output_file, information goods[LEN_STRUCT], int *n)
 		}
 	if (flag)
 	{
-		for (int i = 0; i < *n - 1; i++)
+		for (int i = 0; i < *n; i++)
 			fprintf(inout_file, "%s%s%u\n%u\n", goods[i].name, goods[i].manufacturer, goods[i].cost, goods[i].amount);
-		fprintf(inout_file, "%s%s%u\n%u", goods[*n - 1].name, goods[*n - 1].manufacturer, goods[*n - 1].cost, goods[*n - 1].amount);
 		fclose(inout_file);
 		return SUCCESS;
 	}
@@ -180,9 +167,8 @@ int add_good(char *input_output_file, information goods[LEN_STRUCT], int *n)
 		shift(goods, *n - 1, n);
 		goods[*n - 2] = new_good;
 	}*/
-	for (int i = 0; i < *n - 1; i++)
+	for (int i = 0; i < *n; i++)
 		fprintf(inout_file, "%s%s%u\n%u\n", goods[i].name, goods[i].manufacturer, goods[i].cost, goods[i].amount);
-	fprintf(inout_file, "%s%s%u\n%u", goods[*n - 1].name, goods[*n - 1].manufacturer, goods[*n - 1].cost, goods[*n - 1].amount);
 	fclose(inout_file);
 	return SUCCESS;
 }
@@ -190,13 +176,13 @@ int main(int argc, char **argv)
 {
 	information goods[LEN_STRUCT];
 	int n = 0;
-	if (!argv[1])
+	/*if (!argv[1])
 		return UNKNOWN_PARAMETERS;
 	if (!strcmp(argv[1], "st") && argc == 4)
 		return sorting_goods(argv[2], argv[3], goods, &n);
 	if (!strcmp(argv[1], "ft") && argc == 4)
 		return find_goods(argv[2], argv[3], goods, &n);
-	if (!strcmp(argv[1], "at") && argc == 3)
+	if (!strcmp(argv[1], "at") && argc == 3)*/
 		return add_good(argv[2], goods, &n);
 	return UNKNOWN_PARAMETERS;
 }
