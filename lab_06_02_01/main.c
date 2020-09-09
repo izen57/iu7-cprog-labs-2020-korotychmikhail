483563d9403bb99033b61a31e8661fed46d487e1
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-09-09 22:59:58 +0300

    fix

--- lab_06_02_01/main.c
+++ lab_06_02_01/main.c
@@ -59,15 +59,30 @@ int find_stuff(FILE *file, char *string, information *stuff, int n)
 	fclose(file);
 	return SUCCESS;
 }
-int cmpfunc(const void *a, const void *b) 
-{ 
-	information *i1 = (information *) a;
-	information *i2 = (information *) b;
-	return (i1->weight / i1->volume) - (i2->weight / i2->volume);
-}
+// int cmpfunc(const void *a, const void *b) 
+// { 
+// 	information *i1 = (information *) a;
+// 	information *i2 = (information *) b;
+// 	return (i1->weight / i1->volume) - (i2->weight / i2->volume);
+// }
 int sort_stuff(FILE *file, information *stuff, int n)
 {
-	qsort(stuff, n, sizeof(information), cmpfunc);
+	//qsort(stuff, n, sizeof(information), cmpfunc);
+	int flag;
+	information temp;
+	do
+	{
+		flag = 0;
+		for (int i = 1; i < n; i++)
+			if (stuff[i].weight / stuff[i].volume < stuff[i - 1].weight / stuff[i - 1].volume)
+			{
+				temp = stuff[i];
+				stuff[i] = stuff[i - 1];
+				stuff[i - 1] = temp;
+				flag = 1;
+			}
+	}
+	while (flag);
 	for (int i = 0; i < n; i++)
 		printf("%s\n%f\n%f\n", stuff[i].name, stuff[i].weight, stuff[i].volume);
 	fclose(file);
