a71d22d359dd021f923152f2304f0407cd2a59e6
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-09-11 22:15:39 +0300

    one_exit_add

--- lab_06_02_01/read.c
+++ lab_06_02_01/read.c
@@ -3,25 +3,26 @@
 #include "functions.h"
 int read_stuff(FILE *file, information *stuff, int *n)
 {
+	int flag = 0;
 	for (int i = 0; !feof(file); i++)
 	{
 		if (!fgets(stuff[i].name, LEN_NAME + 2, file))
-			return INCORRECT_INPUT;
+			flag = 1;
 		stuff[i].name[strcspn(stuff[i].name, "\n")] = '\0';
 		if (strlen(stuff[i].name) >= LEN_NAME + 1)
-			return 1;
+			flag = 1;
 		(*n)++;
 		if (fscanf(file, "%f\n", &stuff[i].weight) != 1 || stuff[i].weight <= 0)
-			return INCORRECT_INPUT;
+			flag = 1;
 		(*n)++;
 		if (fscanf(file, "%f\n", &stuff[i].volume) != 1 || stuff[i].volume <= 0)
-			return INCORRECT_INPUT;
+			flag = 1;
 		(*n)++;
 	}
 	if (*n % 3 || !*n || *n > LEN_STRUCT * 3)
-		return INCORRECT_INPUT;
+		flag = 1;
 	else
 		*n /= 3;
 	rewind(file);
-	return SUCCESS;
+	return flag;
 }
