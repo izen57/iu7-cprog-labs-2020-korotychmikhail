5d765d0b3bb4b0baa286eb6ef617df16946df680
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-09-25 17:11:21 +0300

    struct_prog

--- lab_06_02_01/main.c
+++ lab_06_02_01/main.c
@@ -5,19 +5,19 @@ int main(int argc, char **argv)
 	int error = 0;
 	if ((argc == 2 || argc == 3) && argv[1])
 	{
-		printf("8");
+		//printf("8");
 		FILE *file = fopen(argv[1], "r");
 		if (file)
 		{
-			printf("12");
+			//printf("12");
 			information stuff[LEN_STRUCT];
 			int n = 0;
 			if (!read_stuff(file, stuff, &n))
 			{
-				printf("17");
+				//printf("17");
 				if (argc == 2)
 				{
-					printf("20");
+					//printf("20");
 					sort_stuff(stuff, n);
 					for (int i = 0; i < n; i++)
 						printf("%s\n%f\n%f\n", stuff[i].name, stuff[i].weight, stuff[i].volume);
@@ -25,11 +25,11 @@ int main(int argc, char **argv)
 				}
 				else if (argc == 3)
 				{
-					printf("26");
+					//printf("26");
 					error = find_and_print_stuff(argv[2], stuff, n);
 					if (error == 1)
 					{
-						printf("30");
+						//printf("30");
 						for (int i = 0; i < n; i++)
 							printf("%s\n%f\n%f\n", stuff[i].name, stuff[i].weight, stuff[i].volume);
 						error = 0;
