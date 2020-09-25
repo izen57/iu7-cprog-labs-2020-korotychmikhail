15f4a0d8eb7d5d08856ae0a463e5a87ba9443967
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-09-25 16:33:50 +0300

    struct_prog

--- lab_06_02_01/main.c
+++ lab_06_02_01/main.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
 					if (error == 1)
 					{
 						for (int i = 0; i < n; i++)
-							printf("%s\n%f\n%f\n", stuff[i].name, stuff[i].volume, stuff[i].weight);
+							printf("%s\n%f\n%f\n", stuff[i].name, stuff[i].weight, stuff[i].volume);
 						error = 0;
 					}
 					fclose(file);
