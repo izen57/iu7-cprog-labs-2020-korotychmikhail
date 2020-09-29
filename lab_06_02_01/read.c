ef5d49919f27da9e3aba641ddf012a84245c93f0
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-09-29 19:04:57 +0300

    fix

--- lab_06_02_01/read.c
+++ lab_06_02_01/read.c
@@ -7,24 +7,24 @@ int read_stuff(FILE *file, information *stuff, int *n)
 	for (int i = 0; !feof(file); i++)
 	{
 		if (!fgets(stuff[i].name, LEN_NAME + 2, file) || error)
-			error++;
+			error = 1;
 		if (!error)
 			stuff[i].name[strcspn(stuff[i].name, "\n")] = '\0';
 		if (strlen(stuff[i].name) >= LEN_NAME + 1 || error)
-			error++;
+			error = 1;
 		if (!error)
 			(*n)++;
 		if (fscanf(file, "%f\n", &stuff[i].weight) != 1 || stuff[i].weight <= 0 || error)
-			error++;
+			error = 1;
 		if (!error)
 			(*n)++;
 		if (fscanf(file, "%f\n", &stuff[i].volume) != 1 || stuff[i].volume <= 0 || error)
-			error++;
+			error = 1;
 		if (!error)
 			(*n)++;
 	}
 	if (*n % 3 || !*n || *n > LEN_STRUCT * 3 || error)
-		error++;
+		error = 1;
 	else
 		*n /= 3;
 	rewind(file);
