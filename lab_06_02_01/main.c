e489fc9b902bf3776fcc350f7b5a91bccaafb3a7
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-09-29 18:25:17 +0300

    fix

--- lab_06_02_01/main.c
+++ lab_06_02_01/main.c
@@ -10,30 +10,24 @@ int main(int argc, char **argv)
 	int error = 0;
 	if ((argc == 2 || argc == 3) && argv[1])
 	{
-		//printf("8");
 		FILE *file = fopen(argv[1], "r");
 		if (file)
 		{
-			//printf("12");
 			information stuff[LEN_STRUCT];
 			int n = 0;
 			if (!read_stuff(file, stuff, &n))
 			{
-				//printf("17");
 				if (argc == 2)
 				{
-					//printf("20");
 					sort_stuff(stuff, n);
 					print(stuff, n);
 					fclose(file);
 				}
 				else if (argc == 3)
 				{
-					//printf("26");
 					error = find_and_print_stuff(argv[2], stuff, n);
 					if (error == 1)
 					{
-						//printf("30");
 						print(stuff, n);
 						error = 0;
 					}
