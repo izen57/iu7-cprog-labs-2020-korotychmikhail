3675d8d40e81da5ecc379f99c96e1b28a219d9c1
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-10-01 10:21:29 +0300

    memory

--- lab_06_02_01/read.c
+++ lab_06_02_01/read.c
@@ -6,24 +6,24 @@ int read_stuff(FILE *file, information *stuff, int *n)
 	int error = 0;
 	for (int i = 0; !feof(file); i++)
 	{
-		if (!fgets(stuff[i].name, LEN_NAME + 2, file) || error)
+		if (error || !fgets(stuff[i].name, LEN_NAME + 2, file))
 			error = 1;
 		if (!error)
 			stuff[i].name[strcspn(stuff[i].name, "\n")] = '\0';
-		if (strlen(stuff[i].name) >= LEN_NAME && !error)
+		if (!error || strlen(stuff[i].name) >= LEN_NAME)
 			error = 1;
 		if (!error)
 			(*n)++;
-		if (fscanf(file, "%f\n", &stuff[i].weight) != 1 || stuff[i].weight <= 0 || error)
+		if (error || fscanf(file, "%f\n", &stuff[i].weight) != 1 || stuff[i].weight <= 0)
 			error = 1;
 		if (!error)
 			(*n)++;
-		if (fscanf(file, "%f\n", &stuff[i].volume) != 1 || stuff[i].volume <= 0 || error)
+		if (error || fscanf(file, "%f\n", &stuff[i].volume) != 1 || stuff[i].volume <= 0)
 			error = 1;
 		if (!error)
 			(*n)++;
 	}
-	if (*n % 3 || !*n || *n > LEN_STRUCT * 3 || error)
+	if (error || *n % 3 || !*n || *n > LEN_STRUCT * 3)
 		error = 1;
 	else
 	{
