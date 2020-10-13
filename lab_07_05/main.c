9de7f1e39cfec30ebbd7ab2dc657cc18a4fcd61c
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-10-13 14:00:40 +0300

    fix

--- lab_07_05/main.c
+++ lab_07_05/main.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 int counting_numbers(FILE *file, int *n)
 {
+	//printf("18\n");
 	int number, error = 0;
 	for (int i = 0; !feof(file); i++)
 	{
-		if (fscanf(file, "%d", &number) != 1)
+		if (fscanf(file, "%d", &number) == 1)
 			(*n)++;
 		else
 		{
+			//printf("19\n");
 			error = 1;
 			break;
 		}
@@ -17,6 +19,7 @@ int counting_numbers(FILE *file, int *n)
 }
 int read_array(FILE *file, int **begin, int **end)
 {
+	//printf("20\n");
 	int error = 0;
 	while (!feof(file))
 	{
@@ -24,24 +27,42 @@ int read_array(FILE *file, int **begin, int **end)
 		(*end)++;
 	}
 	if (*begin == *end)
+	{
+		//printf("21\n");
+		free(begin);
 		error = 1;
+	}
 	return error;
 }
 int create_arr(int **begin, int count)
 {
+	//printf("22\n");
 	*begin = calloc(count, sizeof(int));
 	if (!*begin)
+	{
+		//printf("23\n");
+		free(begin);
 		return 1;
+	}
 	else
+	{
+		//printf("24\n");
+		free(begin);
 		return 0;
+	}
 }
 int key(const int *pb_src, const int *pe_src, int **pb_dst, int **pe_dst)
 {
 	int error = 0, count = 0;
 	if (!pb_src || !pe_src || !pb_dst || !pe_dst || pb_src == pe_src)
+	{
+		//printf("25\n");
+		free(pb_dst);
 		error = 1;
+	}
 	if (!error)
 	{
+		//printf("26\n");
 		int summ = 0;
 		for (const int *i = pb_src; i < pe_src - 1; i++)
 		{
@@ -51,24 +72,33 @@ int key(const int *pb_src, const int *pe_src, int **pb_dst, int **pe_dst)
 				count++;
 		}
 		if (!count)
+		{
+			//printf("27\n");
+			free(pb_dst);
 			error = 1;
+		}
 		if (!error)
 		{
 			if (create_arr(pb_dst, count))
 			{
+				//printf("28\n");
 				free(pb_dst);
 				error = 1;
 			}
 			else
+			{
+				//printf("29\n");
 				for (const int *i = pb_src; i < pe_src - 1; i++)
 					if (*i > summ)
 						**pe_dst++ = *i;
+			}
 		}
 	}
 	return error;
 }
 int compare(const void *i, const void *j)
 {
+	//printf("30\n");
 	const int *a = i, *b = j;
 	return *a - *b;
 }
@@ -76,9 +106,14 @@ int mysort(void *base, size_t num, size_t size, int compare(const void *, const 
 {
 	int error = 0;
 	if (!base || !num || size != sizeof(int))
+	{
+		//printf("31\n");
+		free(base);
 		error = 1;
+	}
 	else
 	{
+		//printf("32\n");
 		int *temp = NULL, *end = (int *)base + num * size, k = 0;
 		for (int *i = base; i < end; i++)
 		{
@@ -98,64 +133,97 @@ int mysort(void *base, size_t num, size_t size, int compare(const void *, const 
 }
 void output(FILE *file, int *begin, int *end)
 {
+	//printf("33\n");
 	for (int *i = begin; i < end; i++)
 		fprintf(file, "%d ", *i);
 }
 int main(int argc, char **argv)
 {
 	int error = 0;
-	if ((argc == 3 || argc == 4) && argv[1]/* && argv[2]*/) 
+	if ((argc == 3 || argc == 4) && argv[1] && argv[2])
 	{
+		//printf("1\n");
 		FILE *in_file = fopen(argv[1], "r");
 		if (in_file)
 		{
+			//printf("2\n");
 			int n = 0;
 			if (!counting_numbers(in_file, &n))
 			{
+				//printf("3\n");
 				rewind(in_file);
 				int *arr = calloc(n, sizeof(int));
 				int *begin = arr, *end = arr;
 				if (!read_array(in_file, &begin, &end))
 				{
+					//printf("4\n");
 					rewind(in_file);
-					int *newarr = NULL, *endnewarr = newarr, count = n;
+					int *newarr, *pointer = arr, *endnewarr = newarr, count = n;
 					if (argc == 4)
 					{
+						//printf("5\n");
 						if (!key(begin, end, &newarr, &endnewarr))
+						{
+							//printf("6\n");
+							//*pointer = newarr;
 							count = sizeof *newarr / sizeof(int);
+						}
 						else
 						{
+							//printf("7\n");
 							free(arr);
 							error = 1;
 						}
 					}
 					if (!error)
 					{
-						if (mysort(newarr, count, sizeof(int), compare))
+						//printf("8\n");
+						if (mysort(pointer, count, sizeof(int), compare))
+						{
+							//printf("9\n");
 							error = 1;
+						}
 						else
 						{
+							//printf("10\n");
 							FILE *out_file = fopen(argv[2], "w");
 							if (out_file)
+							{
+								//printf("11\n");
 								output(out_file, newarr, endnewarr);
+							}
 							else
+							{
+								//printf("12\n");
 								error = 1;
+							}
 						}
 					}
 				}
 				else
 				{
+					//printf("13\n");
 					free(arr);
 					error = 1;
 				}
 			}
 			else
+			{
+				//printf("14\n");
 				error = 1;
+			}
 		}
 		else
+		{
+			//printf("15\n");
 			error = 1;
+		}
 	}
 	else
+	{
+		//printf("16\n");
 		error = 1;
+	}
+	//printf("17\n");
 	return error;
 }
