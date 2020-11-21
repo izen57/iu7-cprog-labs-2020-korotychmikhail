78792bf36605e482a69fbbdc9bc80ea4edfb94aa
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-11-22 01:02:35 +0300

    fix

--- lab_09_02_01/src/main.c
+++ lab_09_02_01/src/main.c
@@ -22,51 +22,53 @@ int main(int argc, char **argv)
 				error = INCORRECT_INPUT;
 			}
 			else
-				stuff = malloc(n * sizeof(struct information));
-			if (!error && !read_stuff(file, stuff))
 			{
-				if (argc == 2)
+				stuff = malloc(n * sizeof(struct information));
+				if (!error && !read_stuff(file, stuff))
 				{
-					sort_stuff(stuff, n);
-					print(stuff, n);
-					fclose(file);
-					free_information(stuff, n);
-				}
-				else if (argc == 3)
-				{
-					if (!strcmp(argv[2], "ALL"))
+					if (argc == 2)
 					{
+						sort_stuff(stuff, n);
 						print(stuff, n);
-						free_information(stuff, n);
 						fclose(file);
+						free_information(stuff, n);
 					}
-					else
+					else if (argc == 3)
 					{
-						int start = 0, index = 0;
-						while (index != -1 && start < n)
+						if (!strcmp(argv[2], "ALL"))
 						{
-							index = find_stuff(argv[2], stuff, n, start);
-							if (index != -1)
+							print(stuff, n);
+							free_information(stuff, n);
+							fclose(file);
+						}
+						else
+						{
+							int start = 0, index = 0;
+							while (index != -1 && start < n)
 							{
-								printf("%s\n%f\n%f\n", stuff[index].name, stuff[index].weight, stuff[index].volume);
-								start = index + 1;
-							}
-							else
-							{
-								error = INCORRECT_INPUT;
-								fclose(file);
-								free_information(stuff, n);
-								break;
+								index = find_stuff(argv[2], stuff, n, start);
+								if (index != -1)
+								{
+									printf("%s\n%f\n%f\n", stuff[index].name, stuff[index].weight, stuff[index].volume);
+									start = index + 1;
+								}
+								else
+								{
+									error = INCORRECT_INPUT;
+									fclose(file);
+									free_information(stuff, n);
+									break;
+								}
 							}
 						}
 					}
 				}
-			}
-			else
-			{
-				fclose(file);
-				free_information(stuff, n);
-				error = ALLOCATION_ERROR;
+				else
+				{
+					fclose(file);
+					free_information(stuff, n);
+					error = ALLOCATION_ERROR;
+				}
 			}
 		}
 		else
