89a4206a52904fc95db54bdb2f9f59e19a219b54
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-11-23 15:01:40 +0300

    memory?

--- lab_08_02_01/src/task.c
+++ lab_08_02_01/src/task.c
@@ -96,11 +96,11 @@ int counting_minimum(int **matrix, int str, int stb)
 int **add_str_and_stb(int **matrix, int *str, int *stb)
 {
 	matrix = realloc(matrix, ++(*str) * sizeof(int *));
-	//if (!matrix)
-	//{
-		//free_matrix(matrix, *str /*- 1*/);
+	if (!matrix)
+	{
+		free_matrix(matrix, *str /*- 1*/);
 		return NULL;
-	//}
+	}
 	matrix[*str - 1] = calloc(*stb, sizeof(int));
 	if (!matrix[*str - 1])
 	{
