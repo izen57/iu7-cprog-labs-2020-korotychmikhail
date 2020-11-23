3af7a7dd16e7fe270d350617263fb272d4586b94
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-11-23 14:20:54 +0300

    Update sort.h

--- lab_09_02_01/inc/sort.h
+++ lab_09_02_01/inc/sort.h
@@ -1,5 +1,6 @@
 #ifndef __SORT_H__
 #define __SORT_H__
 #include "../inc/structure.h"
+void swap(struct information *stuff, int index1, int index2);
 void sort_stuff(struct information *stuff, int n);
 #endif
