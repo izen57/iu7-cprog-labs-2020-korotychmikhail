3ecc338f10ea02df9e913e7be419df485e4704e5
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-12-19 21:18:48 +0300

    fix

--- lab_11_01_01/main.c
+++ lab_11_01_01/main.c
@@ -173,7 +173,7 @@ int my_snprintf(char *stream, size_t n, const char *format, ...)
 	va_list args;
 	va_start(args, format);
 	int lenght = 0;
-	int spaces, l_count;
+	int /*spaces,*/ l_count;
 	bool specifier = false;
 	const char *current = format;
 	for (int i = 0; current[i] != '\0'; i++)
@@ -273,7 +273,7 @@ int my_snprintf(char *stream, size_t n, const char *format, ...)
 			if (current[i] == '%')
 			{
 				specifier = true;
-				spaces = 0;
+				//spaces = 0;
 				l_count = 0;
 			}
 			else
