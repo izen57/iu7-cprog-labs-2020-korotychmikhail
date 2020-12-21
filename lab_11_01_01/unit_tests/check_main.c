68a0a238c3da5afb8c0d3b5d8479ae0d801279b3
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-12-21 20:34:56 +0300

    fix

--- lab_11_01_01/unit_tests/check_main.c
+++ lab_11_01_01/unit_tests/check_main.c
@@ -12,7 +12,7 @@ END_TEST
 
 START_TEST(test_void_text)
 {
-	char buffer1[13], buffer2[13],  *string1 = "", *string2 = "";
+	char buffer1[13], buffer2[13],  string1[2] = "", string2[2] = "";
 	ck_assert_int_eq(my_snprintf(buffer1, 0, string1), snprintf(buffer2, 0, string2));
 }
 END_TEST
