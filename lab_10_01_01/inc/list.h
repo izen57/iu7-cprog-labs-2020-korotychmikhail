bded369adeaefc24a2a65ff63bdaf505d0f561db
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-12-25 20:10:14 +0300

    fix

--- lab_10_01_01/inc/list.h
+++ lab_10_01_01/inc/list.h
@@ -13,5 +13,6 @@ int comparator(const void *data1, const void *data2);
 node_t *find(node_t *head, const void *data, int (*comparator)(const void *, const void *));
 int copy(node_t *head, node_t **new_head);
 void sorted_insert(node_t **head, node_t *element, int (*comparator)(const void *, const void *));
+node_t *sort(node_t *head, int (*comparator)(const void *, const void *));
 
 #endif
