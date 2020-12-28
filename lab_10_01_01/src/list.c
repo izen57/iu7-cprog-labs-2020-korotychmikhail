83fa4ad373f3ab1fde549a757a13876fdaad5503
Korotych Mikhail <861-kmd19u787@users.noreply.git.iu7.bmstu.ru>
2020-12-28 16:48:32 +0300

    fix

--- lab_10_01_01/src/list.c
+++ lab_10_01_01/src/list.c
@@ -131,10 +131,11 @@ void sorted_insert(node_t **head, node_t *element, int (*comparator)(const void 
 	}
 }
 
-// node_t *sort(node_t *head, int (*comparator)(const void *, const void *))
-// {
-// 	node_t *new_head = NULL;
-// 	for (node_t *current = head; current; current = current->next)
-// 		sorted_insert(&new_head, current, comparator);
-// 	return new_head;
-// }
+node_t *sort(node_t *head, int (*comparator)(const void *, const void *))
+{
+	node_t *new_head = NULL;
+	if (head)
+		for (node_t *current = head; current; current = current->next)
+			sorted_insert(&new_head, current, comparator);
+	return new_head;
+}
